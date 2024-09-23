81d98df6b745d0c490f1935742e336b9e21adae6
Rey Yuuki <irfan31331@gmail.com>
2024-09-23 23:28:44 +0530

    intializing an e-commerce products project

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeProvider } from "@/components/ui/themeProvider";
+import Header from "@/components/header";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,7 +25,15 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+      <Header/>
         {children}
+        </ThemeProvider>
       </body>
     </html>
   );
